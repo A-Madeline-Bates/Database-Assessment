d21dfcfb18f21302e123dd34df87d8f3b3a01027
April Bates <a.madeline.bates@gmail.com>
2021-03-21 14:32:21 +0000

    in the middle of building CMDWhere- fixing the way that datamodel is used before continuing

--- src/DBMain/CommandFiles/CMDSelect.java
+++ src/DBMain/CommandFiles/CMDSelect.java
@@ -5,12 +5,9 @@ import DBMain.ParseExceptions.*;
 
 import java.util.ArrayList;
 
-public class CMDSelect extends CMDType {
+public class CMDSelect extends CMDWhere {
 	private String tableName;
 	private ArrayList<Integer> requestedColumns = new ArrayList<>();
-	private ArrayList<RequestedRow> requestedRows = new ArrayList<>();
-	private ArrayList<RequestedRow> finalRows = new ArrayList<>();
-	DBModelData temporaryModel;
 
 	public void transformModel() throws ParseExceptions {
 		String firstCommand = getNewTokenSafe(DomainType.ATTRIBUTENAME);
@@ -21,10 +18,12 @@ public class CMDSelect extends CMDType {
 				//calling getNewTokenSafe to move past the tablename, which we've already processed and so
 				//don't need here
 				getNewTokenSafe(DomainType.ATTRIBUTENAME);
-				processEndOfString();
+				processWhere(this);
 			}
 		}
 	}
+
+
 
 	/******************************************************
 	 *************  METHOD TO IDENTIFY FILE  **************
@@ -142,228 +141,6 @@ public class CMDSelect extends CMDType {
 			return true;
 		}
 		throw new InvalidCommand(nextCommand, prevCommand, "FROM", null);
-	}
-
-	/******************************************************
-	 ****** METHOD TO END STRING OR TRIGGER 'WHERE' ******
-	 *****************************************************/
-
-	private void processEndOfString() throws ParseExceptions{
-		String nextCommand = getNewTokenSafe(DomainType.UNKNOWN);
-		//if there is no WHERE condition, exit and prepare our print statement
-		if(isThisSemicolon(nextCommand)) {
-			//We are using a normal tokeniser.nextToken() here because we are expecting a NULL
-			String extraInstruction = tokeniser.nextToken();
-			if (isThisCommandEndTHROW(extraInstruction)) {
-				//we want all rows, so set them all
-				requestAllRows();
-				createPrintStatement();
-			}
-		}
-		//if WHERE is called, call our recursive where operation
-		else if(isItWhere(nextCommand)){
-			executeCondition();
-			//the first set of rows we find with WHERE we can consider all relevant
-			finalRows.addAll(requestedRows);
-			recursiveWhereClause();
-		}
-		else{
-			throw new InvalidCommand(nextCommand, "FROM [tablename]", "WHERE", ";");
-		}
-	}
-
-	private boolean isItWhere(String nextCommand){
-		if (nextCommand.equalsIgnoreCase("WHERE")) {
-			return true;
-		}
-		return false;
-	}
-
-	private void requestAllRows(){
-		for(int i=0; i<temporaryModel.getRowNumber(); i++){
-			finalRows.add(RequestedRow.TRUE);
-		}
-	}
-
-	/******************************************************
-	 ************* RECURSIVE 'WHERE' OPERATOR *************
-	 *****************************************************/
-
-	private void recursiveWhereClause() throws ParseExceptions{
-		String nextCommand = getNewTokenSafe(DomainType.UNKNOWN);
-		//if we find a semicolon, exit and prepare our print statement
-		if(isThisSemicolon(nextCommand)) {
-			//We are using a normal tokeniser.nextToken() here because we are expecting a NULL
-			String extraInstruction = tokeniser.nextToken();
-			if (isThisCommandEndTHROW(extraInstruction)) {
-				createPrintStatement();
-			}
-		} else if(nextCommand.equalsIgnoreCase("AND")){
-			//clear requestedRows
-			requestedRows = new ArrayList<RequestedRow>();
-			executeCondition();
-			computeAND();
-			recursiveWhereClause();
-		} else if(nextCommand.equalsIgnoreCase("OR")){
-			//clear requestedRows
-			requestedRows = new ArrayList<RequestedRow>();
-			executeCondition();
-			computeOR();
-			recursiveWhereClause();
-		} else{
-			throw new InvalidCommand(nextCommand, "[WHERE CLAUSE]", "AND, OR", ";");
-		}
-	}
-
-	//computeAND decides which cells from our most recent requestedRows result should make it into our finalRows
-	private void computeAND(){
-		for(int i=0; i<temporaryModel.getRowNumber(); i++) {
-			if((requestedRows.get(i) == RequestedRow.TRUE) && (finalRows.get(i) == RequestedRow.TRUE)){
-				finalRows.set(i, RequestedRow.TRUE);
-			}
-			else{
-				finalRows.set(i, RequestedRow.FALSE);
-			}
-		}
-	}
-
-	//computeOR decides which cells from our most recent requestedRows result should make it into our finalRows
-	private void computeOR(){
-		for(int i=0; i<temporaryModel.getRowNumber(); i++) {
-			if((requestedRows.get(i) == RequestedRow.TRUE) || (finalRows.get(i) == RequestedRow.TRUE)){
-				finalRows.set(i, RequestedRow.TRUE);
-			}
-		}
-	}
-
-	/******************************************************
-	 ****************** CONDITION METHODS *****************
-	 *****************************************************/
-
-	private void executeCondition() throws ParseExceptions {
-		String attributeCommand = getNewTokenSafe(DomainType.ATTRIBUTENAME);
-		//find attribute we're processing- if it doesn't exist, an exception will be thrown
-		int attributeCoordinate = findSingleAttributeTHROW(attributeCommand);
-		String opCommand = getNewTokenSafe(DomainType.OPERATOR);
-		//find the type of our operator- if it's not valid, an exception will be thrown
-		OperatorType opType = returnOperatorType(opCommand);
-		splitByOpType(opType, opCommand, attributeCoordinate);
-	}
-
-	private int findSingleAttributeTHROW(String nextCommand) throws ParseExceptions{
-		//iterate through the columns of our table until we find a match
-		for(int i=0; i<temporaryModel.getColumnNumber(); i++){
-			if(temporaryModel.getColumnData().get(i).equalsIgnoreCase(nextCommand)){
-				return i;
-			}
-		}
-		throw new DoesNotExistAttribute(nextCommand, tableName);
-	}
-
-	/******************************************************
-	 *********** SPLIT CONDITION BY OPERATOR TYPE **********
-	 *****************************************************/
-
-	private void splitByOpType(OperatorType opType, String opCommand, int attributeCoordinate) throws ParseExceptions{
-		String valueCommand = getNewTokenSafe(DomainType.VALUE);
-		//this operation checks whether a value is valid for the operator (throwing an error if not). It then
-		//directs it to the appropriate method.
-		switch(opType){
-			case NUMERICAL:
-				isItNumLiteralTHROW(valueCommand);
-				searchAttributeNum(attributeCoordinate, opCommand, valueCommand);
-				break;
-			case STRING : //we are currently treating '==' and LIKE (i.e STRING and UNIVERSAL) as the same.
-				isItStringLiteralTHROW(valueCommand);
-				searchAttributeUniversal(attributeCoordinate, opCommand, valueCommand);
-				break;
-			default: //default means the opType is universal, so we only need to know if it's a valid value
-				isItValidValue(valueCommand);
-				searchAttributeUniversal(attributeCoordinate, opCommand, valueCommand);
-		}
-	}
-
-	private OperatorType returnOperatorType(String operator) throws ParseExceptions{
-		if(operator.equals("==")){
-			return OperatorType.UNIVERSAL;
-		} else if(operator.equals(">")){
-			return OperatorType.NUMERICAL;
-		} else if(operator.equals("<")){
-			return OperatorType.NUMERICAL;
-		} else if(operator.equals(">=")){
-			return OperatorType.NUMERICAL;
-		} else if(operator.equals("<=")){
-			return OperatorType.NUMERICAL;
-		} else if(operator.equals("!=")){
-			return OperatorType.UNIVERSAL;
-		} else if(operator.equalsIgnoreCase("LIKE")){
-			return OperatorType.STRING;
-		} else{
-			throw new InvalidCommand(operator, "WHERE [attributename]", "[operator]", null);
-		}
-	}
-
-	/******************************************************
-	 ************* NUMERICAL CONDITION METHODS ************
-	 *****************************************************/
-
-	private void searchAttributeNum(int attributeCoordinate, String opCommand, String valueCommand){
-		//create float version of our valueCommand (the number we are using to make our comparison)
-		float comparisonValue = Float.parseFloat(valueCommand);
-		for(int i=0; i<temporaryModel.getRowNumber(); i++){
-			//create a cell for each row and set RequestedRow to false. If the cell does fit the criteria, it will be
-			//set to true later in this loop.
-			requestedRows.add(RequestedRow.FALSE);
-			//if the operation was caught by NumberFormatException our cell value is not a valid number, and so we
-			//want to jump to catch + don't want assignByOperator to consider it
-			try{
-				//consider every cell in our attribute's column
-				float tableValue = Float.parseFloat(temporaryModel.getCell(i, attributeCoordinate));
-				assignByOperator(i, opCommand, tableValue, comparisonValue);
-			} catch(NumberFormatException n){}
-		}
-	}
-
-	private void assignByOperator(int i, String opCommand, float tableValue, float comparisonValue) {
-		switch(opCommand){
-			case ">":
-				if(tableValue > comparisonValue){
-					requestedRows.set(i, RequestedRow.TRUE);
-				} break;
-			case "<":
-				if(tableValue < comparisonValue){
-					requestedRows.set(i, RequestedRow.TRUE);
-				} break;
-			case ">=":
-				if(tableValue >= comparisonValue){
-					requestedRows.set(i, RequestedRow.TRUE);
-				} break;
-			default: //opCommand doesn't equal any of the above, it has to equal "<="
-				if(tableValue <= comparisonValue){
-					requestedRows.set(i, RequestedRow.TRUE);
-				}
-		}
-	}
-
-	/******************************************************
-	 ********* STRING/UNIVERSAL CONDITION METHODS *********
-	 *****************************************************/
-
-	private void searchAttributeUniversal(int attributeCoordinate, String opCommand, String valueCommand){
-		for(int i=0; i<temporaryModel.getRowNumber(); i++){
-			requestedRows.add(RequestedRow.FALSE);
-			if(opCommand.equals("==") || opCommand.equalsIgnoreCase("LIKE")){
-				if (temporaryModel.getCell(i, attributeCoordinate).equals(valueCommand)) {
-					requestedRows.set(i, RequestedRow.TRUE);
-				}
-			}
-			//if opCommand doesn't equal "==" it has to equal "!="
-			else {
-				if (!temporaryModel.getCell(i, attributeCoordinate).equals(valueCommand)) {
-					requestedRows.set(i, RequestedRow.TRUE);
-				}
-			}
-		}
 	}
 
 	private void createPrintStatement(){
--- src/DBMain/CommandFiles/CMDWhere.java
+++ src/DBMain/CommandFiles/CMDWhere.java
@@ -1,6 +1,238 @@
 package DBMain.CommandFiles;
 
+import DBMain.ModelFiles.DBModelData;
+import DBMain.ParseExceptions.*;
+
+import java.util.ArrayList;
+
 public abstract class CMDWhere extends CMDType {
+	protected DBModelData temporaryModel;
+	private ArrayList<RequestedRow> requestedRows = new ArrayList<>();
+	private ArrayList<RequestedRow> finalRows = new ArrayList<>();
 
+	abstract void executeCMD(ArrayList<RequestedRow> finalRows);
 
+	/******************************************************
+	 ****** METHOD TO END STRING OR TRIGGER 'WHERE' ******
+	 *****************************************************/
+
+	protected void processWhere(CMDWhere currentCommand) throws ParseExceptions {
+		String nextCommand = getNewTokenSafe(DomainType.UNKNOWN);
+		//if there is no WHERE condition, exit and prepare our print statement
+		if(isThisSemicolon(nextCommand)) {
+			//We are using a normal tokeniser.nextToken() here because we are expecting a NULL
+			String extraInstruction = tokeniser.nextToken();
+			if (isThisCommandEndTHROW(extraInstruction)) {
+				//we want all rows, so set them all
+				requestAllRows();
+				currentCommand.executeCMD(finalRows);
+				//createPrintStatement();
+			}
+		}
+		//if WHERE is called, call our recursive where operation
+		else if(isItWhere(nextCommand)){
+			executeCondition();
+			//the first set of rows we find with WHERE we can consider all relevant
+			finalRows.addAll(requestedRows);
+			recursiveWhereClause(currentCommand);
+		}
+		else{
+			throw new InvalidCommand(nextCommand, "FROM [tablename]", "WHERE", ";");
+		}
+	}
+
+	protected boolean isItWhere(String nextCommand){
+		if (nextCommand.equalsIgnoreCase("WHERE")) {
+			return true;
+		}
+		return false;
+	}
+
+	protected void requestAllRows(){
+		for(int i=0; i<temporaryModel.getRowNumber(); i++){
+			finalRows.add(RequestedRow.TRUE);
+		}
+	}
+
+	/******************************************************
+	 ************* RECURSIVE 'WHERE' OPERATOR *************
+	 *****************************************************/
+
+	protected void recursiveWhereClause(CMDWhere currentCommand) throws ParseExceptions{
+		String nextCommand = getNewTokenSafe(DomainType.UNKNOWN);
+		//if we find a semicolon, exit and prepare our print statement
+		if(isThisSemicolon(nextCommand)) {
+			//We are using a normal tokeniser.nextToken() here because we are expecting a NULL
+			String extraInstruction = tokeniser.nextToken();
+			if (isThisCommandEndTHROW(extraInstruction)) {
+				currentCommand.executeCMD(finalRows);
+				//createPrintStatement();
+			}
+		} else if(nextCommand.equalsIgnoreCase("AND")){
+			//clear requestedRows
+			requestedRows = new ArrayList<RequestedRow>();
+			executeCondition();
+			computeAND();
+			recursiveWhereClause(currentCommand);
+		} else if(nextCommand.equalsIgnoreCase("OR")){
+			//clear requestedRows
+			requestedRows = new ArrayList<RequestedRow>();
+			executeCondition();
+			computeOR();
+			recursiveWhereClause(currentCommand);
+		} else{
+			throw new InvalidCommand(nextCommand, "[WHERE CLAUSE]", "AND, OR", ";");
+		}
+	}
+
+	//computeAND decides which cells from our most recent requestedRows result should make it into our finalRows
+	protected void computeAND(){
+		for(int i=0; i<temporaryModel.getRowNumber(); i++) {
+			if((requestedRows.get(i) == RequestedRow.TRUE) && (finalRows.get(i) == RequestedRow.TRUE)){
+				finalRows.set(i, RequestedRow.TRUE);
+			}
+			else{
+				finalRows.set(i, RequestedRow.FALSE);
+			}
+		}
+	}
+
+	//computeOR decides which cells from our most recent requestedRows result should make it into our finalRows
+	protected void computeOR(){
+		for(int i=0; i<temporaryModel.getRowNumber(); i++) {
+			if((requestedRows.get(i) == RequestedRow.TRUE) || (finalRows.get(i) == RequestedRow.TRUE)){
+				finalRows.set(i, RequestedRow.TRUE);
+			}
+		}
+	}
+
+	/******************************************************
+	 ****************** CONDITION METHODS *****************
+	 *****************************************************/
+
+	protected void executeCondition() throws ParseExceptions {
+		String attributeCommand = getNewTokenSafe(DomainType.ATTRIBUTENAME);
+		//find attribute we're processing- if it doesn't exist, an exception will be thrown
+		int attributeCoordinate = findSingleAttributeTHROW(attributeCommand);
+		String opCommand = getNewTokenSafe(DomainType.OPERATOR);
+		//find the type of our operator- if it's not valid, an exception will be thrown
+		OperatorType opType = returnOperatorType(opCommand);
+		splitByOpType(opType, opCommand, attributeCoordinate);
+	}
+
+	protected int findSingleAttributeTHROW(String nextCommand) throws ParseExceptions{
+		//iterate through the columns of our table until we find a match
+		for(int i=0; i<temporaryModel.getColumnNumber(); i++){
+			if(temporaryModel.getColumnData().get(i).equalsIgnoreCase(nextCommand)){
+				return i;
+			}
+		}
+		throw new DoesNotExistAttribute(nextCommand, tableName);
+	}
+
+	/******************************************************
+	 *********** SPLIT CONDITION BY OPERATOR TYPE **********
+	 *****************************************************/
+
+	protected void splitByOpType(OperatorType opType, String opCommand, int attributeCoordinate) throws ParseExceptions{
+		String valueCommand = getNewTokenSafe(DomainType.VALUE);
+		//this operation checks whether a value is valid for the operator (throwing an error if not). It then
+		//directs it to the appropriate method.
+		switch(opType){
+			case NUMERICAL:
+				isItNumLiteralTHROW(valueCommand);
+				searchAttributeNum(attributeCoordinate, opCommand, valueCommand);
+				break;
+			case STRING : //we are currently treating '==' and LIKE (i.e STRING and UNIVERSAL) as the same.
+				isItStringLiteralTHROW(valueCommand);
+				searchAttributeUniversal(attributeCoordinate, opCommand, valueCommand);
+				break;
+			default: //default means the opType is universal, so we only need to know if it's a valid value
+				isItValidValue(valueCommand);
+				searchAttributeUniversal(attributeCoordinate, opCommand, valueCommand);
+		}
+	}
+
+	protected OperatorType returnOperatorType(String operator) throws ParseExceptions{
+		if(operator.equals("==")){
+			return OperatorType.UNIVERSAL;
+		} else if(operator.equals(">")){
+			return OperatorType.NUMERICAL;
+		} else if(operator.equals("<")){
+			return OperatorType.NUMERICAL;
+		} else if(operator.equals(">=")){
+			return OperatorType.NUMERICAL;
+		} else if(operator.equals("<=")){
+			return OperatorType.NUMERICAL;
+		} else if(operator.equals("!=")){
+			return OperatorType.UNIVERSAL;
+		} else if(operator.equalsIgnoreCase("LIKE")){
+			return OperatorType.STRING;
+		} else{
+			throw new InvalidCommand(operator, "WHERE [attributename]", "[operator]", null);
+		}
+	}
+
+	/******************************************************
+	 ************* NUMERICAL CONDITION METHODS ************
+	 *****************************************************/
+
+	protected void searchAttributeNum(int attributeCoordinate, String opCommand, String valueCommand){
+		//create float version of our valueCommand (the number we are using to make our comparison)
+		float comparisonValue = Float.parseFloat(valueCommand);
+		for(int i=0; i<temporaryModel.getRowNumber(); i++){
+			//create a cell for each row and set RequestedRow to false. If the cell does fit the criteria, it will be
+			//set to true later in this loop.
+			requestedRows.add(RequestedRow.FALSE);
+			//if the operation was caught by NumberFormatException our cell value is not a valid number, and so we
+			//want to jump to catch + don't want assignByOperator to consider it
+			try{
+				//consider every cell in our attribute's column
+				float tableValue = Float.parseFloat(temporaryModel.getCell(i, attributeCoordinate));
+				assignByOperator(i, opCommand, tableValue, comparisonValue);
+			} catch(NumberFormatException n){}
+		}
+	}
+
+	protected void assignByOperator(int i, String opCommand, float tableValue, float comparisonValue) {
+		switch(opCommand){
+			case ">":
+				if(tableValue > comparisonValue){
+					requestedRows.set(i, RequestedRow.TRUE);
+				} break;
+			case "<":
+				if(tableValue < comparisonValue){
+					requestedRows.set(i, RequestedRow.TRUE);
+				} break;
+			case ">=":
+				if(tableValue >= comparisonValue){
+					requestedRows.set(i, RequestedRow.TRUE);
+				} break;
+			default: //opCommand doesn't equal any of the above, it has to equal "<="
+				if(tableValue <= comparisonValue){
+					requestedRows.set(i, RequestedRow.TRUE);
+				}
+		}
+	}
+
+	/******************************************************
+	 ********* STRING/UNIVERSAL CONDITION METHODS *********
+	 *****************************************************/
+
+	protected void searchAttributeUniversal(int attributeCoordinate, String opCommand, String valueCommand){
+		for(int i=0; i<temporaryModel.getRowNumber(); i++){
+			requestedRows.add(RequestedRow.FALSE);
+			if(opCommand.equals("==") || opCommand.equalsIgnoreCase("LIKE")){
+				if (temporaryModel.getCell(i, attributeCoordinate).equals(valueCommand)) {
+					requestedRows.set(i, RequestedRow.TRUE);
+				}
+			}
+			//if opCommand doesn't equal "==" it has to equal "!="
+			else {
+				if (!temporaryModel.getCell(i, attributeCoordinate).equals(valueCommand)) {
+					requestedRows.set(i, RequestedRow.TRUE);
+				}
+			}
+		}
+	}
 }
